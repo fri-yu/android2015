787f9b74054c721e65b309b2863521fd51844c3c
fri-yu <18768860215@163.com>
2015-09-03 00:10:35 +0800

    在被启动的aty中添加一些html代码进行识别

--- Android workspqce/AdtApp/src/com/example/adtapp/TextViewATY.java
+++ Android workspqce/AdtApp/src/com/example/adtapp/TextViewATY.java
@@ -2,12 +2,20 @@ package com.example.adtapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 public class TextViewATY extends Activity {
+	private TextView textView;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.my_layout);
+		textView = (TextView) findViewById(R.id.tv);
+		String htmlString = "<font color='blue'>it's just a try </font>";
+		CharSequence charSequence = Html.fromHtml(htmlString);
+		textView.setText(charSequence);
 	}
 }
