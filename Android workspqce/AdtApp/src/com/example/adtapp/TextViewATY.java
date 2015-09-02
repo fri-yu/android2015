98b3f3b624ef609b40398d451f2b4c393b385f0a
fri-yu <18768860215@163.com>
2015-09-03 00:42:00 +0800

    通过配置XML文件，使得字符串中的不同连接起作用
    如 web 访问网络 email 邮件 phone  拨号 map 地图等等

--- Android workspqce/AdtApp/src/com/example/adtapp/TextViewATY.java
+++ Android workspqce/AdtApp/src/com/example/adtapp/TextViewATY.java
@@ -8,19 +8,44 @@ import android.widget.TextView;
 
 public class TextViewATY extends Activity {
 	private TextView textView;
-	private TextView myconsole;
+	private TextView textView2;
+	private TextView textViewemail;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.my_layout);
+		InitTextViewByHtmlString();
+		InitTextViewByString();
+		InitTextViewEmail();
+	}
+
+	private void InitTextViewByHtmlString() {
 		textView = (TextView) findViewById(R.id.tv);
-		myconsole = (TextView) findViewById(R.id.myconsole);
+
 		String htmlString = "<font color='blue'>it's just a try </font>";
 		htmlString += "<br/><a href='http://www.baidu.com'>百度一下 你就知道</a>";
 		CharSequence charSequence = Html.fromHtml(htmlString);
 		textView.setText(charSequence);
 		textView.setMovementMethod(LinkMovementMethod.getInstance());
+	}
+
+	private void InitTextViewByString() {
+		textView2 = (TextView) findViewById(R.id.tvweb);
+		String string = "百度一下你就知道 http://www.baidu.com\n"
+				+ "邮箱不可以 157726738@qq.com";
+		textView2.setText(string);
+		textView2.setMovementMethod(LinkMovementMethod.getInstance());
+
+	}
+
+	private void InitTextViewEmail() {
+		textViewemail = (TextView) findViewById(R.id.tvemail);
+		String string = "连接不起作用了 http://www.baidu.com\n"
+				+ "邮箱可以了 157726738@qq.com";
+		textViewemail.setText(string);
+		textViewemail.setMovementMethod(LinkMovementMethod.getInstance());
+
 	}
 }
