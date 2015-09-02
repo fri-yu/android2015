f3676ffda23ff73118990f0e0879a738f77d0763
fri-yu <18768860215@163.com>
2015-09-03 00:26:23 +0800

    论布局的重要性

--- Android workspqce/AdtApp/src/com/example/adtapp/TextViewATY.java
+++ Android workspqce/AdtApp/src/com/example/adtapp/TextViewATY.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 
 public class TextViewATY extends Activity {
 	private TextView textView;
+	private TextView myconsole;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +15,11 @@ public class TextViewATY extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.my_layout);
 		textView = (TextView) findViewById(R.id.tv);
+//		myconsole = (TextView) findViewById(R.id.myconsole);
 		String htmlString = "<font color='blue'>it's just a try </font>";
-		htmlString+="<br/><a href='http://www.baidu.com'>百度一下 你就知道</a>";
+		htmlString += "<br/><a href='http://www.baidu.com'>百度一下 你就知道</a>";
 		CharSequence charSequence = Html.fromHtml(htmlString);
-		textView.setText(charSequence);
+		textView.setText("123");
+//		textView.setMovementMethod(LinkMovementMethod.getInstance());
 	}
 }
