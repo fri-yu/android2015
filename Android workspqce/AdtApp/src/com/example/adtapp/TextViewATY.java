baa01460d3d63062d6c783c1c02129ca737cc0d0
fri-yu <18768860215@163.com>
2015-09-03 00:27:35 +0800

    可以点击链接了哦

--- Android workspqce/AdtApp/src/com/example/adtapp/TextViewATY.java
+++ Android workspqce/AdtApp/src/com/example/adtapp/TextViewATY.java
@@ -3,6 +3,7 @@ package com.example.adtapp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
 
 public class TextViewATY extends Activity {
@@ -15,11 +16,11 @@ public class TextViewATY extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.my_layout);
 		textView = (TextView) findViewById(R.id.tv);
-//		myconsole = (TextView) findViewById(R.id.myconsole);
+		myconsole = (TextView) findViewById(R.id.myconsole);
 		String htmlString = "<font color='blue'>it's just a try </font>";
 		htmlString += "<br/><a href='http://www.baidu.com'>百度一下 你就知道</a>";
 		CharSequence charSequence = Html.fromHtml(htmlString);
-		textView.setText("123");
-//		textView.setMovementMethod(LinkMovementMethod.getInstance());
+		textView.setText(charSequence);
+		textView.setMovementMethod(LinkMovementMethod.getInstance());
 	}
 }
