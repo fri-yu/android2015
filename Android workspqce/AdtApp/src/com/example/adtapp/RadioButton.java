5ee9ff4f9a7ce05091d4610bb17c47c975d2110e
fri-yu <18768860215@163.com>
2015-09-03 07:54:23 +0800

    选择单选框，并赋值给textview

--- Android workspqce/AdtApp/src/com/example/adtapp/RadioButton.java
+++ Android workspqce/AdtApp/src/com/example/adtapp/RadioButton.java
@@ -1,11 +1,19 @@
 package com.example.adtapp;
 
+import android.R.integer;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class RadioButton extends Activity {
 	private Button buttonDefult;
+	private TextView textView;
+	private RadioGroup radiog;
+	String textString;
 	int flag = 1;
 
 	@Override
@@ -18,7 +26,22 @@ public class RadioButton extends Activity {
 	}
 
 	private void Init() {
+		buttonDefult = (Button) findViewById(R.id.btn);
+		textView = (TextView) findViewById(R.id.textview);
+		textString = textView.getText().toString();
+		radiog = (RadioGroup) findViewById(R.id.radiog);
+		buttonDefult.setOnClickListener(new OnClickListener() {
 
+			@Override
+			public void onClick(View arg0) {
+				int a = radiog.getCheckedRadioButtonId();
+				if (a != -1) {
+					// TODO Auto-generated method stub
+					android.widget.RadioButton rButton = (android.widget.RadioButton) findViewById(a);
+					textView.setText(textString + "\n" + rButton.getText());
+				}
+			}
+		});
 	}
 
 }
