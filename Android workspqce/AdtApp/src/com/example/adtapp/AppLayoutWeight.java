4f65f1bfff72b8e6e62e6160ab2f6f72111a9cbd
fri-yu <18768860215@163.com>
2015-09-04 20:02:25 +0800

    已经可以获得重绘后的button的width了，其他的请补充

--- Android workspqce/AdtApp/src/com/example/adtapp/AppLayoutWeight.java
+++ Android workspqce/AdtApp/src/com/example/adtapp/AppLayoutWeight.java
@@ -11,6 +11,8 @@ import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewTreeObserver;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
@@ -28,19 +30,29 @@ public class AppLayoutWeight extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.appweight);
 		Init();
-
+		
 	}
 
 	private void Init() {
-		Button btn1 = (Button) findViewById(R.id.button1);
+		final Button btn1 = (Button) findViewById(R.id.button1);
 		Button btn2 = (Button) findViewById(R.id.button2);
 		Button btn3 = (Button) findViewById(R.id.button3);
 		Button btn4 = (Button) findViewById(R.id.button4);
-		TextView txv=(TextView)findViewById(R.id.txv);
-		int a=btn1.getWidth();
+		final TextView txv=(TextView)findViewById(R.id.txv);
 		
-		txv.setText(a+"");
-		
+		ViewTreeObserver vto = btn1.getViewTreeObserver();   
+		vto.addOnGlobalLayoutListener(new OnGlobalLayoutListener() { 
+		    @Override   
+		    public void onGlobalLayout() { 
+		    	btn1.getViewTreeObserver().removeGlobalOnLayoutListener(this); 
+		    	btn1.getHeight();
+		        btn1.getWidth();
+		        int a=btn1.getWidth();
+				
+				txv.setText(a+"");
+		    }   
+		}); 
+
 //		btn1.setText(btn1.getWidth());
 //		btn2.setText(btn2.getWidth());
 //		btn3.setText(btn3.getWidth());
