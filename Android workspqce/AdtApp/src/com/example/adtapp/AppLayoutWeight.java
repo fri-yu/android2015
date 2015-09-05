683f42d770ccde3110f47e168e9214bc5f8ca65a
fri-yu <18768860215@163.com>
2015-09-05 14:15:34 +0800

    AdtApp update

--- Android workspqce/AdtApp/src/com/example/adtapp/AppLayoutWeight.java
+++ Android workspqce/AdtApp/src/com/example/adtapp/AppLayoutWeight.java
@@ -30,32 +30,69 @@ public class AppLayoutWeight extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.appweight);
 		Init();
-		
+
 	}
 
 	private void Init() {
 		final Button btn1 = (Button) findViewById(R.id.button1);
-		Button btn2 = (Button) findViewById(R.id.button2);
-		Button btn3 = (Button) findViewById(R.id.button3);
-		Button btn4 = (Button) findViewById(R.id.button4);
-		final TextView txv=(TextView)findViewById(R.id.txv);
-		
-		ViewTreeObserver vto = btn1.getViewTreeObserver();   
-		vto.addOnGlobalLayoutListener(new OnGlobalLayoutListener() { 
-		    @Override   
-		    public void onGlobalLayout() { 
-		    	btn1.getViewTreeObserver().removeGlobalOnLayoutListener(this); 
-		    	btn1.getHeight();
-		        btn1.getWidth();
-		        int a=btn1.getWidth();
-				
-				txv.setText(a+"");
-		    }   
-		}); 
+		final Button btn2 = (Button) findViewById(R.id.button2);
+		final Button btn3 = (Button) findViewById(R.id.button3);
+		final Button btn4 = (Button) findViewById(R.id.button4);
+		final TextView txv = (TextView) findViewById(R.id.txv);
 
-//		btn1.setText(btn1.getWidth());
-//		btn2.setText(btn2.getWidth());
-//		btn3.setText(btn3.getWidth());
-//		btn4.setText(btn4.getWidth());
+		ViewTreeObserver vto = btn1.getViewTreeObserver();
+		vto.addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
+			@Override
+			public void onGlobalLayout() {
+				btn1.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+				btn1.getHeight();
+				btn1.getWidth();
+				int a = btn1.getWidth();
+
+				txv.setText(a + "");
+			}
+		});
+		ViewTreeObserver vto4 = btn4.getViewTreeObserver();
+		vto4.addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
+			@Override
+			public void onGlobalLayout() {
+				btn4.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+				btn4.getHeight();
+				btn4.getWidth();
+				int a = btn4.getWidth();
+
+				txv.setText(txv.getText() + "-" + a + "");
+			}
+		});
+		ViewTreeObserver vto3 = btn3.getViewTreeObserver();
+		vto3.addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
+			@Override
+			public void onGlobalLayout() {
+				btn3.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+				btn3.getHeight();
+				btn3.getWidth();
+				int a = btn3.getWidth();
+
+				txv.setText(txv.getText() + "-" + a + "");
+			}
+		});
+
+		ViewTreeObserver vto2 = btn2.getViewTreeObserver();
+		vto2.addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
+			@Override
+			public void onGlobalLayout() {
+				btn2.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+				btn2.getHeight();
+				btn2.getWidth();
+				int a = btn2.getWidth();
+				btn2.setText(a+"");
+//				txv.setText(txv.getText() + "-" + a + "");
+			}
+		});
+
+		// btn1.setText(btn1.getWidth());
+		// btn2.setText(btn2.getWidth());
+		// btn3.setText(btn3.getWidth());
+		// btn4.setText(btn4.getWidth());
 	}
 }
