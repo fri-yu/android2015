a95c2ca9cb08ef933fcd9410e026365e38b3b188
fri-yu <18768860215@163.com>
2015-09-05 20:12:36 +0800

    添加touchListener工程

---
+++ Android workspqce/TouchListener/src/com/example/touchlistener/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.touchlistener;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.provider.ContactsContract.CommonDataKinds.Event;
+import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.FrameLayout;
+
+public class MainActivity extends Activity {
+	private FrameLayout fLayout;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		findViewById(R.id.container).setOnTouchListener(new OnTouchListener() {
+
+			@Override
+			public boolean onTouch(View arg0, MotionEvent arg1) {
+				switch (arg1.getAction()) {
+				case MotionEvent.ACTION_DOWN:
+					System.out.println("down ");
+					break;
+				case MotionEvent.ACTION_UP:
+					System.out.println("UP ");
+					break;
+				case MotionEvent.ACTION_MOVE:
+					System.out.println("MOVE ");
+					break;
+
+				default:
+					break;
+				}
+				return false;
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
